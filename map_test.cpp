c318e202bcf498d3311c266f78825eead7ce3b04
Katarina Vorne <kvorne@nd.edu>
2022-02-23 18:18:46 -0500

    Vector within map testing

--- map_test.cpp
+++ map_test.cpp
@@ -7,9 +7,8 @@ int main()
 {
     std::map<std::string, int> m { {"David", 10}, {"Chris", 15}, {"Kat", 20}, {"Aidan", 5}};
     //std::map<std::string, std::map<std::string, int> > mapSquared {{"hello", m}, {"panda", m}};
-    std::vector<char> vect;
-     vect.assign('a','b');
-    std::vector<char> vect2;
+    std::vector<char> vect = {'a','b'};
+    std::vector<char> vect2 = {'c','d'};
      vect.assign('c','d');
     std::map<int, std::vector<char> > vectMap {{1, vect}, {2, vect2}};
     for (auto i : m)
