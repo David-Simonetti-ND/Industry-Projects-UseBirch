3ac2db8379d297cdd1188e6a9b7ef77c90a0590b
Katarina Vorne <kvorne@nd.edu>
2022-02-23 18:24:43 -0500

    added UseBirchMap

--- map_test.cpp
+++ map_test.cpp
@@ -9,8 +9,8 @@ int main()
     //std::map<std::string, std::map<std::string, int> > mapSquared {{"hello", m}, {"panda", m}};
     std::vector<char> vect = {'a','b'};
     std::vector<char> vect2 = {'c','d'};
-     vect.assign('c','d');
-    std::map<int, std::vector<char> > vectMap {{1, vect}, {2, vect2}};
+    std::map<int, std::vector<char> > vectMap = {{1, vect}, {2, vect2}};
+    std::map<std::string, std::vector<std::string> > useBirchMap = {{"one", {"a", "b", "c", "d"}}, {"two", {"e", "f", "g", "h"}}};
     for (auto i : m)
     {
         std::cout << i.first << " " << i.second << std::endl;
