f504a516789dd78e3e64bfdd1ff623b5fe5a0d92
Katarina Vorne <kvorne@nd.edu>
2022-02-24 10:27:58 -0500

    Trying original vector parsing

--- vector_test.cpp
+++ vector_test.cpp
@@ -9,7 +9,6 @@ int main(){
     int nums[5]={1,2,3,4,5};
     int nums2[5]={6,7,8,9,10};
     std::vector<char> charVect = {'a','b','c','d'};
-    std::vector<int*> arrVect = {nums, nums2};
     std::vector<std::string> stringVect = {"Hello", "Goodbye"};
     std::vector<std::vector<int> > vectorSquared = {{1,2,3}, {4,5,6}, intVect};
     intVect.push_back(5);
