15c64ee2cda30d470a363415703a276fcea75b50
Christopher Boumalhab <cboumalh@nd.edu>
2022-02-24 13:52:28 -0500

    changes in vector_test fixed

--- vector_test.cpp
+++ vector_test.cpp
@@ -9,7 +9,7 @@ int main(){
     std::vector<char> charVect = {'a','b','c','d'};
     charVect.push_back('e');
     std::vector<std::string> stringVect = {"Hello", "Goodbye"};
-    std::vector<std::vector<std::string>> v1 = {{"Hi", "Some", "DHSIJFDSBGBGUFGEWFB"}}
+    std::vector<std::vector<std::string>> v1 = {{"Hi", "Some", "DHSIJFDSBGBGUFGEWFB"}};
     std::vector<std::vector<int> > vectorSquared = {{1,2,3}, {4,5,6}, intVect};
     intVect.push_back(5);
     intVect.pop_back();
