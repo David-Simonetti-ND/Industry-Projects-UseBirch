df89843b6637b242aca22931d2c4075c0681b22e
Katarina Vorne <kvorne@nd.edu>
2022-02-24 10:34:41 -0500

    Trying original vector parsing

--- map_test.cpp
+++ map_test.cpp
@@ -6,7 +6,7 @@
 int main()
 {
     std::map<std::string, int> m { {"David", 10}, {"Chris", 15}, {"Kat", 20}, {"Aidan", 5}};
-    std::map<std::string, std::map<std::string, int> > mapSquared = {{"hello", m}, {"panda", m}};
+    //std::map<std::string, std::map<std::string, int> > mapSquared = {{"hello", m}, {"panda", m}};
     std::vector<char> vect = {'a','b'};
     std::vector<char> vect2 = {'c','d'};
     std::map<int, std::vector<char> > vectMap = {{1, vect}, {2, vect2}};
--- vector_test.cpp
+++ vector_test.cpp
@@ -6,8 +6,6 @@
 int main(){
     std::vector<int> intVect;
     intVect.assign(1,3);
-    int nums[5]={1,2,3,4,5};
-    int nums2[5]={6,7,8,9,10};
     std::vector<char> charVect = {'a','b','c','d'};
     std::vector<std::string> stringVect = {"Hello", "Goodbye"};
     std::vector<std::vector<int> > vectorSquared = {{1,2,3}, {4,5,6}, intVect};
