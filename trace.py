4db3cf59c82f7c876fa6b3cf85ed477c0988fa5f
Christopher Boumalhab <cboumalh@nd.edu>
2022-03-31 11:09:05 -0400

    15 seconds and creating file before redirection

--- trace.py
+++ trace.py
@@ -30,8 +30,9 @@ return_value = None
 args = {}
 command_line_args = []
 def check_infiniteloop():
+    os.mknod("time.txt")
     time_process = subprocess.Popen(f'(time {sys.argv[1]}) &> time.txt', shell = True, preexec_fn=os.setsid)
-    time.sleep(10)
+    time.sleep(15)
     run_time = open('time.txt')
     if not list(run_time.read().splitlines()):
         print("There may be an infinite loop in the code")
